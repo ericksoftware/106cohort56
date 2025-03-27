a651948623b6ac2c46c3c23dbb9237afe84b265e
ericksoftware <ericklopezrl5qqq@gmail.com>
2025-03-27 14:58:29 -0700

    Assignment 2

--- script/app.js
+++ script/app.js
@@ -1,14 +1,23 @@
 
-function sayHello(){
-    console.log("Hello sayhello");
-}
+// function sayHello(){
+//     console.log("Hello sayhello");
+// }
 
-let toy="hello";
+// let toy="hello";
+
+// function init(){
+//     console.log("Hello in the init");
+// }
+
+function saveTask(){
+    console.log("saveTask is running");
+}
 
 function init(){
-    console.log("Hello in the init");
-}
+    console.log("The init it's running");
 
+    $("#btnSave").click(saveTask)
+}
 
 
 window.onload = init;
